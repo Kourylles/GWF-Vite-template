6c604f01013a0f578b9c64cd65d0800832548ed6
kourylles <38153882+Kourylles@users.noreply.github.com>
2022-07-21 18:32:27 +0200

    Ajout d'un exemple de l'utilsation du store

---
+++ src/components/PresentationStore.vue
@@ -0,0 +1,38 @@
+<script setup>
+  const props = defineProps({
+  exempleStore : { type: Object, required: true}
+});
+</script>
+
+<template>
+  <div class="q-ma-xl">
+    <q-markup-table separator="cell">
+      <colgroup>
+        <col style="width: 500px" />
+        <col />
+      </colgroup>
+      <thead class="bg-teal">
+        <tr>
+          <th colspan="2">
+            <div class="row no-wrap-items-center">
+              <q-img
+                src="@/assets/logo.png"
+                spinner-color="white"
+                style="width: 70px"
+                class="rounded-borders"
+              />
+              <div class="text-h4 q-ml-md text-white">Données du Store</div>
+            </div>
+          </th>
+        </tr>
+      </thead>
+      <tbody class="bg-grey-3 text-body1 text-left">
+        <tr v-for="item in exempleStore.dataExemple" :key="item.nom">
+          <td>{{ item.prenom }}</td>
+          <td>{{ item.nom }}</td>
+        </tr>
+      </tbody>
+    </q-markup-table>
+  </div>
+</template>
+<style lang="scss" scoped></style>
--- src/pages/Accueil.vue
+++ src/pages/Accueil.vue
@@ -2,13 +2,21 @@
 /****************************************
  *              IMPORTS                 *
  * **************************************/
+// PINIA
+import useExempleStore from '@/stores/ExempleStore.js'
 // Components
 import PresentationTemplate from "@/components/PresentationTemplate.vue";
+import PresentationStore from "@/components/PresentationStore.vue";
+
+// Récupération du Store
+const exempleStore = useExempleStore();
+
 </script>
 
 <template>
   <div>
-    <Presentation-Template />
+    <PresentationTemplate />
+    <PresentationStore :exempleStore = "exempleStore" />
   </div>
 </template>
 <style scoped></style>
